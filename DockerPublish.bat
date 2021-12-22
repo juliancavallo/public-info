2b27d1a233a415b078f191912e3f4879a4780b47
Julian Cavallo <julianpaul241@hotmail.com>
2021-12-22 00:13:41 -0300

    Docker bat update

--- DockerPublish.bat
+++ DockerPublish.bat
@@ -1,10 +1,17 @@
 @echo off
 
+copy Dockerfile PublicInfo.API
+
 cd PublicInfo.API
 cmd /C docker build -f ./Dockerfile --force-rm -t publicinfoimg:dev ..
+del "Dockerfile"
 
 cd ../
 cmd /C heroku container:push -a public-info  web
 cmd /C heroku container:release -a public-info web
 
+docker image prune -a -f
+
 pause
+
+
