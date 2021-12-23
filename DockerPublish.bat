619b54f5c7d5bb5c8dcb4d0b7b28db19844d8fc0
Julian Cavallo <julianpaul241@hotmail.com>
2021-12-22 23:17:03 -0300

    Swagger redirect

--- DockerPublish.bat
+++ DockerPublish.bat
@@ -1,12 +1,7 @@
 @echo off
 
-copy Dockerfile PublicInfo.API
+cmd /C docker build -t publicinfoimg ..
 
-cd PublicInfo.API
-cmd /C docker build -f ./Dockerfile --force-rm -t publicinfoimg:dev ..
-del "Dockerfile"
-
-cd ../
 cmd /C heroku container:push -a public-info  web
 cmd /C heroku container:release -a public-info web
 
--- PublicInfo.API/Startup.cs
+++ PublicInfo.API/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -47,6 +48,10 @@ namespace PublicInfo.API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PublicInfo v1"));
             }
+
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
 
             app.UseHttpsRedirection();
 
